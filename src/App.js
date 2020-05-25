8ffc8af3e9d582f2c7a5f74ea644b3c2cee9a5d1
Matt Dominick <mdominick300@gmail.com>
2020-05-25 19:37:07 -0400

    adjustments

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Start from "./pages/Start";
+import Search from "./pages/Search";
 import Navbar from "./components/Navbar";
 import Wrapper from "./components/Wrapper";
 import './App.css';
@@ -12,6 +13,7 @@ function App() {
         <Navbar />
         <Wrapper>
           <Route exact path="/" component={Start} />
+          <Route exact path="/search" component={Search} />
         </Wrapper>
       </div>
     </Router>
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,16 +1,15 @@
 import React from "react";
 import "./style.css";
 import Footer from "../components/Footer";
-// import { Link } from "react-router-dom";
 
 
-function Start() {
+function Search() {
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>Search</h1>
       <Footer></Footer>
     </div>
   );
 }
 
-export default Start;
+export default Search;
